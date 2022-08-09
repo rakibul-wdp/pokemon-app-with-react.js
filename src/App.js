7511a45e630e683d8f038b0a95123206fd91ada6
rakibul-wdp <rakibul.wdp@gmail.com>
2022-08-09 16:53:34 +0600

    add navbar

--- src/App.js
+++ src/App.js
@@ -1,9 +1,10 @@
 import './App.css';
+import Navbar from './components/Navbar';
 
 function App() {
   return (
     <div className=''>
-      <button class='btn btn-primary'>Button</button>
+      <Navbar />
     </div>
   );
 }
